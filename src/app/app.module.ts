e8a9230259bfa2cba94a789884cb74e4099fef9f
Deni Ninkovic <deni.ninkovic@codecentric.de>
2020-03-20 16:43:29 +0100

    Install bootstrap and jquery JS file for bootstrap animations. And change style file from sass to scss.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {TooltipModule} from 'ngx-bootstrap/tooltip';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { AboutComponent } from './dashboard/about/about.component';
-import { SkillsComponent } from './dashboard/skills/skills.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {AboutComponent} from './dashboard/about/about.component';
+import {SkillsComponent} from './dashboard/skills/skills.component';
 
 @NgModule({
     declarations: [
@@ -17,8 +16,7 @@ import { SkillsComponent } from './dashboard/skills/skills.component';
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule,
-        TooltipModule.forRoot(),
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
